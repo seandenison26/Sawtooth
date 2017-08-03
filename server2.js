377d5eb98557292521211bbc977d489c24d9af65
ziggy <sean.denison26@gmail.com>
2017-08-02 19:36:21 -0500

    Logged the store

--- server2.js
+++ server2.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const react = require("redux");
 
 const app = express();
+
+console.log(react.createStore);
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+	console.log('Example app listening on port 3000!')
 })
